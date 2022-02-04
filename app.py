15da60bedf04222a9799135bdd1c873fd736825b
schollar <cschollar22@gmail.com>
2022-02-04 01:42:51 -0700

    Changed up requests to interact with a DB. Added dbhandler functions to deal with DB interactions

--- app.py
+++ app.py
@@ -1,34 +1,41 @@
 from flask import Flask, request, Response
 import json
+import dbhandler as dbh
+db = dbh.dbInteraction()
 app = Flask(__name__)
+
+# GET REQUEST stores animal list being returned from get_animals function, then we dump it to json, and return our response with the json we just converted.
 
 
 @app.get('/animals')
 def animals():
+    animals_list = db.get_animals()
     animals_json = json.dumps(animals_list, default=str)
     return Response(animals_json, mimetype="application/json", status=200)
 
 
 @app.post('/animals')
 def add_animal():
-    user_animal = request.json['animal']
-    animals_list.append(user_animal)
+    animal_name = request.json['name']
+    animal_desc = request.json['description']
+    db.add_animal(animal_name, animal_desc)
     return Response("You've successfully added an animal", mimetype="plain/text", status=200)
 
 
 @app.patch('/animals')
 def change_animal():
-    animals_list[1] = "Large Dog"
+    animal_name = request.json['name']
+    new_name = request.json['new_name']
+    new_description = request.json['description']
+    db.change_animal(animal_name, new_name, new_description)
     return Response("You've successfully changed an animal", mimetype="plain/text", status=200)
 
 
 @app.delete('/animals')
 def remove_animal():
-    animals_list.remove("Cat")
+    animal_name = request.json['name']
+    db.delete_animal(animal_name)
     return Response("You've successfully deleted an animal", mimetype="plain/text", status=200)
-
-
-animals_list = ["Cat", "Dog", "Gorilla"]
 
 
 app.run(debug=True)
---
+++ dbhandler.py
@@ -0,0 +1,81 @@
+import dbcreds
+import mariadb as db
+
+
+class dbInteraction:
+    # Connect function that starts a DB connection and creates a cursor
+    def db_connect(self):
+        conn = None
+        cursor = None
+        try:
+            conn = db.connect(user=dbcreds.user, password=dbcreds.password,
+                              host=dbcreds.host, port=dbcreds.port, database=dbcreds.database)
+            cursor = conn.cursor()
+        except db.OperationalError:
+            print('Something is wrong with the DB')
+        except:
+            print('Something went wrong connecting to the DB')
+        return conn, cursor
+# Disconnect function that takes in the conn and cursor and attempts to close both
+
+    def db_disconnect(self, conn, cursor):
+        try:
+            cursor.close()
+        except:
+            print('Error closing cursor')
+        try:
+            conn.close()
+        except:
+            print('Error closing connection')
+
+    def get_animals(self):
+        animals = []
+        conn, cursor = self.db_connect()
+        try:
+            cursor.execute(
+                "SELECT name, description FROM animals")
+            animals = cursor.fetchall()
+        except db.OperationalError:
+            print('Something is wrong with the db!')
+        except db.ProgrammingError:
+            print('Error running DB query')
+        self.db_disconnect(conn, cursor)
+
+        return animals
+
+    def add_animal(self, animal_name, animal_desc):
+        conn, cursor = self.db_connect()
+        try:
+            cursor.execute(
+                "INSERT INTO animals (name, description) VALUES (?, ?)", [animal_name, animal_desc])
+
+        except db.OperationalError:
+            print('Something is wrong with the db!')
+        except db.ProgrammingError:
+            print('Error running DB query')
+        conn.commit()
+        self.db_disconnect(conn, cursor)
+
+    def change_animal(self, animal_name, new_name, new_description):
+        conn, cursor = self.db_connect()
+        try:
+            cursor.execute(
+                "UPDATE animals SET name = ?, description = ? WHERE name = ?", [new_name, new_description, animal_name])
+        except db.OperationalError:
+            print('Something is wrong with the db!')
+        except db.ProgrammingError:
+            print('Error running DB query')
+        conn.commit()
+        self.db_disconnect(conn, cursor)
+
+    def delete_animal(self, animal_name):
+        conn, cursor = self.db_connect()
+        try:
+            cursor.execute(
+                "DELETE FROM animals WHERE name = ?", [animal_name, ])
+        except db.OperationalError:
+            print('Something is wrong with the db!')
+        except db.ProgrammingError:
+            print('Error running DB query')
+        conn.commit()
+        self.db_disconnect(conn, cursor)
